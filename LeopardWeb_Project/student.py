5814df3dde113c7840fcdb83326bb0d03713e0bc
hillhatwit <hillh@wit.edu>
2025-06-24 14:43:57 +0000

    check for me

--- LeopardWeb_Project/student.py
+++ LeopardWeb_Project/student.py
@@ -6,10 +6,25 @@ class Student(User):
     def __init__(self, in_firstName, in_lastName, in_id):
         User.__init__(self, in_firstName, in_lastName, in_id)
         
+    
     #methods
-    def search_courses(self):
-          return "This it the function for students to search courses"
-    
-        
+    def search_courses(self,Searchh_keyword,search_value):
 
-    
+        cx = sqlite3.connect("assignment3.db")
+        cursor = cx.cursor()
+
+        # Use parameterized queries to prevent SQL injection and handle types correctly
+        query = f"SELECT * FROM COURSES WHERE {Searchh_keyword} = {search_value}"
+        cursor.execute(query)
+        rows = cursor.fetchall()
+
+        for row in rows:
+            print(row)
+
+        #return "This it the function for students to search courses"
+
+studen = Student("Hill","Hill",1)
+studen.search_courses("CRN", "2500")
+
+
+
