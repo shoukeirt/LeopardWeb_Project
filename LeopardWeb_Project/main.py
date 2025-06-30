71fba2c8e44cc6d55ef6a0204aa20c8da6515dbe
hillhatwit <hillh@wit.edu>
2025-06-30 15:31:40 -0400

    Update main.py

--- LeopardWeb_Project/main.py
+++ LeopardWeb_Project/main.py
@@ -1,64 +1,204 @@
 import sqlite3
-import os
-import user
-import student
-import instructor 
-import admin
-import login_menu
-cx = sqlite3.connect("assignment3.db")
-cursor = cx.cursor()
-              
-def main():
-    # cursor.execute("SELECT * FROM STUDENT")
-    # rows = cursor.fetchall();
+from user import User
+import datetime
 
-    # for row in rows:
-    #     print(row)
+#test push to git
+def slipt_time(time_str):
+
+    start_time, end_time = time_str.split('-')
+    start_time = datetime.datetime.strptime(start_time.strip(), '%I:%M%p')
+    end_time = datetime.datetime.strptime(end_time.strip(), '%I:%M%p')
+    return start_time, end_time
+
+
+class Student(User):
     
-    # cursor.execute("SELECT * FROM INSTRUCTOR")
-    # rows = cursor.fetchall()
+    def __init__(self, in_firstName, in_lastName, in_id):
+        User.__init__(self, in_firstName, in_lastName, in_id)
 
-    # for row in rows:
-    #     print(row)
+
         
-    # sql_command = """INSERT INTO student VALUES(10011, 'ANTHONY', 'MAGLIOZZI', 2026, "BSCE",'Magliozzia');"""
-    # #cursor.execute(sql_command)
-    # cursor.execute("SELECT * FROM STUDENT")
-    # # rows = cursor.fetchall();
-    # sql_command = """DELETE FROM COURSES WHERE CRN = 4"""
+    def search_courses(self, Search_keyword, search_value):
+        cx = sqlite3.connect("../LeopardWeb_Project/LeopardWeb_Project/test.db")  # This has to be changed to the correct DB File 
+        cursor = cx.cursor()
 
-    # # cursor.execute(sql_command)
-    # for row in rows:
-    #     print(row)
+        # Check if the student exists in the database
+        sql_command = "SELECT * FROM STUDENT WHERE ID = ? and NAME = ? and SURNAME = ?"
+        cursor.execute(sql_command, (self.id,self.firstName, self.lastName))
+        student = cursor.fetchone()
+        if not student:
+            print("Student not found.")
+            cx.close()
+            return []
         
-    # # cursor.execute("PRAGMA table_info(STUDENT);")
-    # # for col in cursor.fetchall():
-    # #     print(col)
-    
-    # # print("USING:", os.path.abspath("assignment3.db"))
-  
-    # student1 = student.Student("Hill","Hill",1)
-    # student1.search_courses("CRN", "2500")
-    # new_admin1 = admin.Admin("First","Admin", 30003, cursor, cx)
 
-    # new_admin1.add_course()
-    # new_admin1.remove_course()
-    # new_admin1.search_courses("TITLE", "Advanced Digital Circuit Design")
-    # new_admin1.add_user()
-    # new_admin1.add_instructor()
-    # new_admin1.add_to_course()
-    # new_admin1.remove_from_course()
-    # new_admin1.link_prof()
-    # new_admin1.unlink_prof()
 
-    login_menu.login(cursor,cx)
-    cx.commit() 
+        if Search_keyword.upper() == "CRN":
+            Search_keyword = "CRN"
+        elif Search_keyword.upper() in ["COURSE NAME", "TITLE"]:
+            Search_keyword = "TITLE"
+        else:
+            print("Invalid search keyword.")
+            return []
 
-# close the connection 
-    cx.close() 
+        query = f"SELECT * FROM COURSES WHERE {Search_keyword} = ?"
+        cursor.execute(query, (search_value,))
+        rows = cursor.fetchall()
 
-main()
+        for row in rows:
+            print(row)
+        return rows
 
 
 
 
+
+
+
+    def add_course(self, CRN):
+        cx = sqlite3.connect("../LeopardWeb_Project/LeopardWeb_Project/test.db")  # Adjust the path to your database file
+        cursor = cx.cursor()
+
+        
+        # Check if the student exists in the database
+        sql_command = "SELECT * FROM STUDENT WHERE ID = ? and NAME = ? and SURNAME = ?"
+        cursor.execute(sql_command, (self.id,self.firstName, self.lastName))
+        student = cursor.fetchone()
+        if not student:
+            print("Student not found.")
+            cx.close()
+            return []
+        
+
+
+        # query to check the course exists
+        query = "SELECT * FROM COURSES WHERE CRN = ?"
+        cursor.execute(query, (CRN,))
+        course = cursor.fetchone()
+        # query to check if the student is already enrolled in the course
+        sql_command = "SELECT * FROM ENROLLMENT WHERE STUDENT_ID = ?"
+        cursor.execute(sql_command, (self.id,))
+        enrollment = cursor.fetchall()
+
+        #if the course does not exist, print an error message
+        if not course:
+            print("Course not found.")
+            cx.close()
+            return
+        # 
+        else:
+            enrolled_crns = [enr[1] for enr in enrollment]
+            if enrolled_crns:
+                # check for time conflicts with already enrolled courses
+                query = f"SELECT * FROM COURSES WHERE CRN IN ({','.join(['?'] * len(enrolled_crns))})"
+                cursor.execute(query, enrolled_crns)
+                enrolled_courses = cursor.fetchall()
+
+                #parse the course days and times
+                course_days = course[4]
+                course_hour_start, course_hour_end = slipt_time(course[3])
+
+                #check for time conflicts with already enrolled courses through loops
+                for enrolled_course in enrolled_courses:
+                    enrolled_days = enrolled_course[4]
+                    enrolled_hour_start, enrolled_hour_end = slipt_time(enrolled_course[3])
+                    if any(day in enrolled_days for day in course_days) and not (course_hour_end <= enrolled_hour_start or course_hour_start >= enrolled_hour_end):
+                        print(f"Time conflict with course {enrolled_course[0]}: {enrolled_course[1]}")
+                        cx.close()
+                        return
+            #if there is no time conflict, add the course to the enrollment table   
+            query = "INSERT INTO ENROLLMENT (STUDENT_ID, CRN) VALUES (?, ?)"
+            cursor.execute(query, (self.id, course[0]))
+            cx.commit()
+            print(f"Course {course[0]} added successfully.")
+        cx.close()
+
+
+
+    def remove_course(self, CRN):
+        cx = sqlite3.connect("../LeopardWeb_Project/LeopardWeb_Project/test.db")  # REMEMBER TO FIX THIS LATER
+        cursor = cx.cursor()
+
+        # Check if the student exists in the database
+        sql_command = "SELECT * FROM STUDENT WHERE ID = ? and NAME = ? and SURNAME = ?"
+        cursor.execute(sql_command, (self.id,self.firstName, self.lastName))
+        student = cursor.fetchone()
+        if not student:
+            print("Student not found.")
+            cx.close()
+            return []
+        
+
+
+
+
+
+        #querry to chek if the student is enrolled in the course
+        sql_command = "SELECT * FROM ENROLLMENT WHERE STUDENT_ID = ? AND CRN = ?"
+        cursor.execute(sql_command, (self.id, CRN))
+        enrollment = cursor.fetchone()
+
+        #check if the student is enrolled in the course
+        if not enrollment:
+            print("You are not enrolled in this course.")
+            cx.close()
+        else:
+            query = "DELETE FROM ENROLLMENT WHERE STUDENT_ID = ? AND CRN = ?"
+            cursor.execute(query, (self.id, CRN))  
+            cx.commit()
+            print(f"Course with CRN {CRN} removed successfully.")         
+        cx.close()
+
+
+    def print_courses(self):
+        cx = sqlite3.connect("../LeopardWeb_Project/LeopardWeb_Project/test.db")  # FIX LATTER
+        cursor = cx.cursor()
+
+        # Check if the student exists in the database
+        sql_command = "SELECT * FROM STUDENT WHERE ID = ? and NAME = ? and SURNAME = ?"
+        cursor.execute(sql_command, (self.id,self.firstName, self.lastName))
+        student = cursor.fetchone()
+        if not student:
+            print("Student not found.")
+            cx.close()
+            return []
+        
+
+
+
+        query = """
+        SELECT * FROM ENROLLMENT
+        WHERE ENROLLMENT.STUDENT_ID = ?
+        """
+        cursor.execute(query, (self.id,))
+        rows = cursor.fetchall()
+        course = []
+
+        #use the enrolled courses to get the course details
+        for row in rows:
+            query = "SELECT * FROM COURSES WHERE CRN = ?"
+            cursor.execute(query, (row[1],))
+            course.append( cursor.fetchall())
+        print("Courses enrolled:")
+        #display the relevant course details
+        for course in course:
+            print("Name: ",course[0][1]," Department:", course[0][2]," Time:", course[0][3],"Days: ", course[0][4])
+        cx.close()
+        return rows   
+
+
+
+if __name__ == "__main__":
+
+    student= Student("Toufic", "Shoukeir", 10015)
+    student.remove_course(2500)
+    student.remove_course(2750)
+    student.remove_course(4670)
+    student.add_course(2500)
+    student.add_course(2750)
+    student.add_course(4670)
+
+    student.print_courses()
+
+    for i in range(0, 5):
+            print('Test')
